15100b811e5280453fc6949c1b5cfb1d5bef1611
HJYH012 <HJYH012@163.com>
2025-08-21 20:10:53 +0800

    Create time.js

---
+++ JavaScript/time.js
@@ -0,0 +1,18 @@
+function displayTime()
+{
+    var week=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    var date=new Date(Date.now());
+    var hours=date.getHours();
+    var minutes=date.getMinutes();
+    var seconds=date.getSeconds();
+    var year=date.getFullYear();
+    var month=date.getMonth()+1;
+    var day=date.getDate();
+    month=(month<10?"0":"")+month;
+    day=(day<10?"0":"")+day;
+    hours=(hours<10?"0":"")+hours;
+    minutes=(minutes<10?"0":"")+minutes;
+    seconds=(seconds<10?"0":"")+seconds;
+    document.getElementById("clock").innerHTML=year+"/"+month+"/"+day+"("+week[date.getDay()]+")"+hours+":"+minutes+":"+seconds+"&nbsp;&nbsp;";
+    setTimeout(displayTime,1);
+}
